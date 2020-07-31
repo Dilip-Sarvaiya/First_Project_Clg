28646c802553fe342a33036db901d436098dd063
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-07-31 22:19:07 +0530

    Contact Us Screen is Added

---
+++ app/src/main/java/com/rku/first_project_clg/Contact_us.java
@@ -0,0 +1,14 @@
+package com.rku.first_project_clg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contact_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
