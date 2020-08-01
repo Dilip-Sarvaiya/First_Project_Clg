fa5561d8d8102c4d6a6eda19fb48893a1c6a7c02
Dilip-Sarvaiya <dsarvaiya248@rku.ac.in>
2020-08-01 10:32:51 +0530

    Three Screen are Added

--- app/src/main/java/com/rku/first_project_clg/Login_Screen.java
+++ app/src/main/java/com/rku/first_project_clg/Login_Screen.java
@@ -9,13 +9,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class Contact_us extends AppCompatActivity {
+public class Login_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_us);
+        setContentView(R.layout.activity_login);
     }
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.custom_menu,menu);
         return super.onCreateOptionsMenu(menu);
@@ -26,24 +27,24 @@ public class Contact_us extends AppCompatActivity {
 
         switch (item.getItemId()){
             case R.id.mnuSplash:
-                Intent intentContact=new Intent(Contact_us.this,Splash_Screen.class);
-                startActivity(intentContact);
+                Intent intentsplash=new Intent(Login_Screen.this,Splash_Screen.class);
+                startActivity(intentsplash);
                 Toast.makeText(this, "Splash Clicked", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.mnuLogin:
-                Intent intentlogin=new Intent(Contact_us.this,Login_Screen.class);
-                startActivity(intentlogin);
                 Toast.makeText(this, "Login Clicked", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.mnuAbout:
-                Intent intentabout=new Intent(Contact_us.this,About_us.class);
+                Intent intentabout=new Intent(Login_Screen.this,About_us.class);
                 startActivity(intentabout);
                 Toast.makeText(this, "About Clicked", Toast.LENGTH_SHORT).show();
                 break;
 
             case R.id.mnuContact:
+                Intent intentContact=new Intent(Login_Screen.this,Contact_us.class);
+                startActivity(intentContact);
                 Toast.makeText(this, "Contact Clicked", Toast.LENGTH_SHORT).show();
                 break;
         }
